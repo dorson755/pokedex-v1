9549d0946d6231c304b799faf38b1e32691ba21d
Dorson Williams <68614535+dorson755@users.noreply.github.com>
2025-05-14 04:45:32 +0000

    tailwind works perfectly...

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-600">
+      <h1 className="text-white text-4xl font-bold">Tailwind Works 💥</h1>
+    </div>
+  )
+}
